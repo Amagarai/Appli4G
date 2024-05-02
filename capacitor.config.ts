84590991bd56728a36d8d719ee0e69954c42de29
Amagaraï Guindo <amagarai6@gmail.com>
2024-05-02 12:35:16 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Appli4G',
+  webDir: 'www'
+};
+
+export default config;
